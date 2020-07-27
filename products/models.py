8d4e02d7d10a55651525745074e4420c594f0e44
Romeo-Peter <romeomeshack34@gmail.com>
2020-07-27 09:39:32 +0100

    Product component

---
+++ products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Product(models.Model):
+    """Product items"""
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    """Order for product item"""
+    Product = models.ForeignKey(Product, max_length=200, blank=True, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
