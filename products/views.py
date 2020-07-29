f21908392aed893fc6826112569439589066a211
Romeo-Peter <romeomeshack34@gmail.com>
2020-07-29 22:03:39 +0100

    Products views

--- products/views.py
+++ products/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
-from .models import Product
-
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
 
+from .models import Product
 from .serializer import ProductSerializer
 
-class ProductsListView(viewsets.ViewSet):
+
+class ProductsViewSet(viewsets.ViewSet):
     """List products viewset"""
+
     def list(self, request):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
-
