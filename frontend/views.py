97e374ba0e5d620d53f64e6881466ca6ca4240e0
Romeo-Peter <romeomeshack34@gmail.com>
2020-07-29 22:21:39 +0100

    React index file rendered in Django template

--- frontend/views.py
+++ frontend/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     context = {
-        'title': 'Django rest framework with react',
+        "title": "Django rest framework with react",
     }
-    return render(request, 'frontend/index.html', context)
+    return render(request, "frontend/index.html", context)
