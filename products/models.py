efc79f5e8e54ad0f4fb5fe78547e5bc081e08e10
Romeo-Peter <romeomeshack34@gmail.com>
2020-07-29 21:58:19 +0100

    Model class

--- products/models.py
+++ products/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
+
 class Product(models.Model):
     """Product items"""
+
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    image = models.CharField(max_length=200, null=True, blank=True)
+    image = models.FileField(upload_to="uploads/%Y/%m/%d", null=True, blank=True)
     price = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+
 class Order(models.Model):
     """Order for product item"""
-    product = models.ForeignKey(Product, max_length=200, blank=True, on_delete=models.DO_NOTHING)
+
+    product = models.ForeignKey(
+        Product, max_length=200, blank=True, on_delete=models.DO_NOTHING
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
