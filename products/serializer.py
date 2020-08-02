d38f5e5d341b41185804bd954dd6ccf5da308818
Romeo-Peter <romeomeshack34@gmail.com>
2020-08-02 23:36:18 +0100

    updated serializer to return image field url

--- products/serializer.py
+++ products/serializer.py
@@ -5,9 +5,15 @@ from .models import Product, Order
 class ProductSerializer(serializers.ModelSerializer):
     """Products model serializer"""
 
+    image = serializers.SerializerMethodField("get_image_url")
+
     class Meta:
         model = Product
         fields = "__all__"
+
+    def get_image_url(self, obj):
+        request = self.context["request"]
+        return request.build_absolute_uri(obj.image.url)
 
 
 class OrderSerializer(serializers.ModelSerializer):
