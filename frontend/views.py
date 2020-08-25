48c09553d1e91a176e8162aa8fa3a183a9bcc885
Romeo-Peter <romeomeshack34@gmail.com>
2020-08-25 20:59:30 +0100

    Added CSRF decorator to view

--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+
 
 from products.models import Product
 
 # Create your views here.
 
 
+@ensure_csrf_cookie
 def index(request):
     context = {"title": "Django Paystack"}
     return render(request, "frontend/index.html", context)
