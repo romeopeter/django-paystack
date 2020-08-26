0962e64baff993e6d2f107e6b459b59f50ef26b1
Romeo-Peter <romeomeshack34@gmail.com>
2020-08-26 06:58:14 +0100

    Included cart urls in base urls

--- base/urls.py
+++ base/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("frontend.urls")),
     path("api/", include("products.urls")),
+    path("carts/", include("cart.urls")),
 ]
 
 if settings.DEBUG:
