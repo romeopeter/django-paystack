1ff5aaf472986e368c0617eeb2c788070ad2e2d0
Romeo-Peter <romeomeshack34@gmail.com>
2020-08-26 07:00:27 +0100

    Cart views. Handles user's cart and checkout

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def cart(request):
+    json_response = {}
+    json_response["message"] = "This is test response"
+    json_response["error"] = "This is a test for error"
+
+    return HttpResponse(json.dump(json_response))
+
+
+def checkout():
+    pass
