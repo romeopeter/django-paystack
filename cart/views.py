83a906a8fef8d6d7acea7eb0526c786b8436c3e5
Romeo-Peter <romeomeshack34@gmail.com>
2020-08-28 18:03:53 +0100

    Process cart request -- still in progress

--- cart/views.py
+++ cart/views.py
@@ -1,15 +1,55 @@
 import json
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
+
+from cart.models import Cart
+
+app_name = "cart"
 
 
 def cart(request):
-    json_response = {}
-    json_response["message"] = "This is test response"
-    json_response["error"] = "This is a test for error"
+    if request.method == "POST":
+        cart_items = request.body
 
-    return HttpResponse(json.dump(json_response))
+        if cart_items.decode("utf-8") == "No Cart":
+            return JsonResponse(
+                {
+                    "statusCode": 200,
+                    "statusText": "success",
+                    "message": "preflight cart",
+                },
+            )
+
+        cart_items = json.loads(cart_items.decode("utf-8"))
+        cart_object = Cart.objects.all()
+
+        for item in cart_items:
+            if cart_object.filter(pk=item["productID"]).exists():
+                return JsonResponse(
+                    {
+                        "statusCode": 400,
+                        "statusText": "Erorr",
+                        "message": "Item has already been saved",
+                    }
+                )
+
+            save_carts = Cart.objects.create(
+                product_image_url=["productImage"], product_subtotal=["productPrice"]
+            )
+
+            if save_carts.saved():
+                return JsonResponse(
+                    {"status": 200, "statusText": "success", "message": "Cart saved"}
+                )
+
+    return JsonResponse(
+        {
+            "statusCode": 404,
+            "statusText": "Bad Request",
+            "message": "GET request sent, expecting POST",
+        }
+    )
 
 
 def checkout():
