9c4959560bed8c4d4da3ba0d29d440231d51c671
Romeo-Peter <romeomeshack34@gmail.com>
2020-09-07 09:40:58 +0100

    Added user API account endpoint to project level url

--- base/urls.py
+++ base/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("api/v1/", include("products.urls")),
     path("api/v1/carts/", include("cart.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/v1/", include("account.urls")),
+    path("api/v1/rest-auth/", include("accounts.urls")),
 ]
 
 if settings.DEBUG:
