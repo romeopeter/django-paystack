d1599264ac33f7f9dfedb0c4e19411e61ac6947a
Romeo-Peter <romeomeshack34@gmail.com>
2020-09-07 22:17:24 +0100

    Modified component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,12 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
-import Store from "./Store";
-import Checkout from "./Checkout";
+import Home from "./store/Home";
 
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 export default function App() {
-  return (
-    <Router>
-      <Fragment>
-        <Switch>
-          <Route path="/" exact component={Store} />
-          {/*<Route path="/checkout/:product_id" component={Checkout} />*/}
-        </Switch>
-      </Fragment>
-    </Router>
-  );
+  return <Home />;
 }
 
 const container = document.getElementById("app");
