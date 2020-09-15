a05f56c150ecc05bd6e8e563f9c371bd4ecdf6d9
Romeo-Peter <romeomeshack34@gmail.com>
2020-09-15 08:51:57 +0100

    Modified action creator to take in payload

--- frontend/src/redux/actions/actionCreators.js
+++ frontend/src/redux/actions/actionCreators.js
@@ -3,13 +3,13 @@ import { ADD_ITEM_TO_CART, DELETE_ITEM_FROM_CART } from "./actionTypes";
 function addItemToCart(item) {
   return {
     type: ADD_ITEM_TO_CART,
-    item,
+    payload: item,
   };
 }
 
 function deleteItemFromCart(item_id) {
   return {
-    type: ADD_ITEM_TO_CART,
-    item_id,
+    type: DELETE_ITEM_FROM_CART,
+    payload: item_id,
   };
 }
