1f4dcb335f90fdab4925e3cbe0e04ea745e47c81
Romeo-Peter <romeomeshack34@gmail.com>
2020-09-15 14:15:40 +0100

    Added initial state to store

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,4 +1,12 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
 
-export default createStore(rootReducer);
+const initialState = {
+  cart: [],
+};
+
+export default createStore(
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
