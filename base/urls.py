3c48022dadaff17a547ddb3a85fa4191ca03257f
Romeo-Peter <romeomeshack34@gmail.com>
2020-10-04 16:27:27 +0100

    Comment for endpoints

--- base/urls.py
+++ base/urls.py
@@ -5,9 +5,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Render templates endpoint
     path("", include("frontend.urls")),
+    # Render API data endpoint
     path("api/v1/", include("products.urls")),
     path("api/v1/carts/", include("cart.urls")),
+    # API authentication endpoint
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/rest-auth/", include("accounts.urls")),
 ]
