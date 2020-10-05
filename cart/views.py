d639fa1d0f4b54dd45c0037a1669284d3c8dd90e
Romeo-Peter <romeomeshack34@gmail.com>
2020-10-05 01:46:45 +0100

    DRF API class-based view

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,32 @@
-from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
+from rest_framework import authentication
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Cart
 from .serializer import CartSerializer
 
+from products.serializer import ProductSerializer
 
-def cart(request):
-    pass
+
+class AddItemToCart(APIView):
+    """
+    View to add item to cart
+
+    * Requires authentication
+    """
+
+    serializer_class = CartSerializer
+
+    # authentication_classes = [authentication.TokenAuthentication]
+
+    def post(self, request, format=None):
+        """
+        Add product to cart
+        """
+
+        serializer = CartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error, status=status.HTTP_401_BAD_REQUEST)
