444d1cd310f1de251540b5c3d18386cca23e2019
Romeo-Peter <romeomeshack34@gmail.com>
2020-10-05 01:50:32 +0100

    Overrode the default model save method

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import post_save
 
 from django.contrib.auth.models import User
 
@@ -8,12 +7,12 @@ from products.models import Product
 
 # Create your models here.
 class Cart(models.Model):
-    """Cart class model"""
+    """Cart model table"""
 
     user = models.ForeignKey(
-        User, verbose_name="cart owner", on_delete=models.DO_NOTHING
+        User, null=True, verbose_name="cart owner", on_delete=models.DO_NOTHING
     )
-    product = models.ManyToManyField(Product)
+    product_name = models.CharField(max_length=100, null=True)
     product_image_url = models.CharField(max_length=200, null=True)
     product_subtotal = models.DecimalField(
         default=0.00, max_digits=100, decimal_places=2, null=True
@@ -23,34 +22,12 @@ class Cart(models.Model):
     )
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if self.product_subtotal > 0:
+            calculate_for_tax = float(self.product_subtotal) * float(1.8)
+            self.product_total = calculate_for_tax
+
+            super().save(*args, **kwargs)
+
     def __str__(self):
         return self.product_name
-
-
-def prouduct_m2m_change_reciever(sender, action, instance, *args, **kwargs):
-    if action == "post_add" or action == "post_removed" or action == "post.clear":
-        product_fields = instance.product.all()
-
-        sub_price = 0
-        image_url = ""
-
-        for field in product_fields:
-            sub_price += field.price
-            image_url += field.product_image_url
-
-        instance.product_subtotal = sub_price
-        instance.product_image_url = image_url
-        instance.save()
-
-
-m2m_changed.connect(prouduct_m2m_change_reciever, sender=Cart)
-
-
-def cart_product_total_reciever(sender, instance, *args, **kwargs):
-    if instance.product_subtotal > 0:
-        calculate_for_tax = float(instance.product_subtotal) * float(1.8)
-        instance.product_total = calculate_for_tax
-    instance.product_total = 0.00
-
-
-pre_save.connect(cart_product_total_reciever, sender=Cart)
