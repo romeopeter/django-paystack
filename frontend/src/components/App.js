796ecf0e4d0c3dec7969d3b2e30228c8f1c593aa
Romeo-Peter <romeomeshack34@gmail.com>
2020-10-08 14:27:05 +0100

    React parent component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "../redux/reducers/rootReducer";
 
 import Home from "./store/Home";
-
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Men from "./store/Men";
+import Women from "./store/Women";
 
 export default function App() {
   return <Home />;
@@ -16,7 +18,11 @@ export default function App() {
 const container = document.getElementById("app");
 ReactDOM.render(
   <Provider store={createStore(rootReducer)}>
-    <App />
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/men" component={Men} />
+      <Route path="/women" component={Women} />
+    </Router>
   </Provider>,
   container
 );
