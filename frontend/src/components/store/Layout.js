4419086385eb684c3fe8940ae9847cbf1784e67b
Romeo-Peter <romeomeshack34@gmail.com>
2020-10-08 15:04:28 +0100

    Page layout

--- frontend/src/components/store/Layout.js
+++ frontend/src/components/store/Layout.js
@@ -1,23 +1,33 @@
 import React, { Fragment } from "react";
 
+import { Link } from "react-router-dom";
+
 export default function Layout({ children }) {
   const footer = (
     <div className="text-center text-md p-5 shadow-sm" id="creator-credit">
       <h4>
-        Created by <a href="#">Romeo Peter</a>
+        Created by <a href="https://romeopeter.com">Romeo Peter</a>
       </h4>
       <div id="contact-icon">
         <i>
-          <a href="https://upwork.com/RomeoPeter">UpWork</a>
+          <a href="https://upwork.com/RomeoPeter">
+            <i>UpWork</i>
+          </a>
         </i>
         <i>
-          <a href="https://gitub.com/Romeo-Peter">Github</a>
+          <a href="https://gitub.com/Romeo-Peter">
+            <i class="fab fa-github-alt"></i>
+          </a>
         </i>
         <i>
-          <a href="https://twitter.com/_romeopeter">Twitter</a>
+          <a href="https://twitter.com/_romeopeter">
+            <i class="fab fa-twitter"></i>
+          </a>
         </i>
         <i>
-          <a href="https://instagram.com/_romeopeter">Instagram</a>
+          <a href="https://instagram.com/_romeopeter">
+            <i class="fab fa-instagram"></i>
+          </a>
         </i>
       </div>
     </div>
@@ -27,15 +37,19 @@ export default function Layout({ children }) {
     <Fragment>
       <nav>
         <h1 id="logo" className="font-bold text-md text-black lead-tight mb-5">
-          <a href="/">Showroom</a>
+          <Link to="/">Showroom</Link>
         </h1>
         <ul>
-          <li>Men</li>
-          <li>Women</li>
+          <li>
+            <Link to="/men">Men</Link>
+          </li>
+          <li>
+            <Link to="/women">Women</Link>
+          </li>
         </ul>
         <div className="cart">
-          {/*<i class="fas fa-shopping-cart"></i>*/}
-          Carts 0
+          {/*<i className="fas fa-shopping-cart"></i>*/}
+          <Link to="/checkout">Carts 0</Link>
         </div>
       </nav>
       <main>{children}</main>
