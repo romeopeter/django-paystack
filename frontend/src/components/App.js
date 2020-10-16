84bac28cfba5e827a767c27159dce1e9c53a118e
Romeo-Peter <romeomeshack34@gmail.com>
2020-10-17 00:25:30 +0100

    Base component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,24 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { Provider } from "react-redux";
+// Redux
 import { createStore } from "redux";
+import { Provider } from "react-redux";
+import store from "../redux/store/store";
 import rootReducer from "../redux/reducers/rootReducer";
 
+// Components
 import Home from "./store/Home";
 import Men from "./store/Men";
 import Women from "./store/Women";
 import Cart from "./store/Cart";
 
-export default function App() {
-  return <Home />;
-}
-
 const container = document.getElementById("app");
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
+  <Provider store={store}>
     <Router>
       <Route path="/" exact component={Home} />
       <Route path="/men" component={Men} />
