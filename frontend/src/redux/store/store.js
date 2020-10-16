366284ea87c8f3a1b1d4fb966caff1f6ce28f21d
Romeo-Peter <romeomeshack34@gmail.com>
2020-10-17 00:20:23 +0100

    Refactored redux store code

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,12 +1,21 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
 import rootReducer from "../reducers/rootReducer";
+
+import thunk from "redux-thunk";
+
+import composeWithDevTools from "redux-devtools-extension";
+
+const middleware = [thunk];
 
 const initialState = {
   cart: [],
 };
 
-export default createStore(
+const store = createStore(
   rootReducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(...middleware))
 );
+
+export default store;
