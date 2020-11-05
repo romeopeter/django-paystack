9ecac515efdabea4b87ec2b85ae7b78babc03474
Romeo-Peter <romeomeshack34@gmail.com>
2020-11-05 21:51:35 +0100

    Added redux composeEnhancers

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import rootReducer from "../reducers/rootReducer";
 
 import thunk from "redux-thunk";
 
-import composeWithDevTools from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const middleware = [thunk];
 
@@ -12,10 +12,11 @@ const initialState = {
   cart: [],
 };
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
