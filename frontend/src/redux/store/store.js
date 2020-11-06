d0fe5062bc752dcdf9d3ed8dfeb40270bd0f84d6
Romeo-Peter <romeomeshack34@gmail.com>
2020-11-06 12:33:57 +0100

    Redux with devtool NPM package

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -16,7 +16,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
